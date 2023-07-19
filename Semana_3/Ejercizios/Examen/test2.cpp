f40cc7dfa8b0c6e4aaf71918e18ae4d37a4358a4
Giuliano <giuliandominici@gmail.com>
2023-07-19 02:08:47 +0200

    exercize

---
+++ Semana_3/Ejercizios/Examen/test2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m;
+	int p(4);
+	int n(10);
+	bool test1(true);
+	bool test2(test1 and (p < n));
+
+	for (m = n/2; m < n; ++m) {
+		if (test1 and test2){
+			cout << p + m << " ";
+			test2 = not test2;
+		}else {
+			cout << n + m << " ";
+			test1 = not test1;
+		}
+	}
+	if (test1 and test2){
+		cout << "1 ";
+	}else {
+		cout << "2 ";
+	}
+}
