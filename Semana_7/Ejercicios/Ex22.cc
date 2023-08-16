12db657a7c35c0398099daa7f32ed8cac4c2e43a
Gogiu23 <giuliandominici@gmail.com>
2023-08-16 16:02:53 +0200

    miércoles, 16 de agosto de 2023, 16:02:53 CEST

--- Semana_7/Ejercicios/Ex22.cc
+++ Semana_7/Ejercicios/Ex22.cc
@@ -7,9 +7,26 @@ struct Casa {
 
 struct Persona {
 	string name;
-
+	Casa* home;
 };
 
+void affiche(struct Persona p)
+{
+	cout << p.name << " vive en " << (p.home)->direccion << endl;
+}
+
 int main() {
+	Casa m1 = { "Barcelona" };
+	Persona p1 = {"Giuliano", &m1};
+	Persona p2 = {"Alessia", &m1};
+
+	Casa m2 = {"Italia"};
+	Persona p3 = {"Loredana", &m2};
+	Persona p4 = {"Ginebra", &m2};
+	affiche(p1); affiche(p2); affiche(p3); affiche(p4);
+
+	p1.home = &m2;
+	cout << "Mientras tanto: ";
+	affiche(p1); affiche(p2); affiche(p3); affiche(p4);
 	return 0;
 }
